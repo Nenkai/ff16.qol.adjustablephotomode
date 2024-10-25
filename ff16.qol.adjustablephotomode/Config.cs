b5ad836d47b784c97377b4fc8d263fff84ba645b
Nenkai <Nenkai@users.noreply.github.com>
2024-10-26 00:45:50 +0200

    Add project files.

---
+++ ff16.qol.adjustablephotomode/Config.cs
@@ -0,0 +1,54 @@
+﻿using ff16.qol.adjustablephotomode.Template.Configuration;
+
+using Reloaded.Mod.Interfaces.Structs;
+
+using System.ComponentModel;
+
+namespace ff16.qol.adjustablephotomode.Configuration;
+
+public class Config : Configurable<Config>
+{
+    [DisplayName("Collision Sphere Radius")]
+    [Description("Radius of the collision box (originally 2.5)")]
+    [DefaultValue(10000.0f)]
+    public float CollisionSphereRadius { get; set; } = 10000.0f;
+
+    [DisplayName("Camera Move Speed")]
+    [Description("Camera Move Speed (originally 2.5)")]
+    [DefaultValue(10.0)]
+    public float CameraMoveSpeed { get; set; } = 10.0f;
+
+    [DisplayName("Camera Up/Down Speed")]
+    [Description("Camera Up/Down Speed (originally 0.5)")]
+    [DefaultValue(2.0f)]
+    public float CameraUpDownSpeed { get; set; } = 2.0f;
+
+    [DisplayName("Camera Rotation Speed")]
+    [Description("Camera Rotation Speed (originally 45)")]
+    [DefaultValue(45.0f)]
+    public float CameraRotationSpeed { get; set; } = 45.0f;
+
+    [DisplayName("Field of View (FOV)")]
+    [Description("Field of View (originally 45)")]
+    [DefaultValue(45.0f)]
+    public float FieldOfView { get; set; } = 45.0f;
+
+    [DisplayName("Field of View Min (FOV)")]
+    [Description("Field of View Minimum (originally 10)")]
+    [DefaultValue(10.0f)]
+    public float FieldOfViewMin { get; set; } = 10.0f;
+
+    [DisplayName("Field of View Min (FOV)")]
+    [Description("Field of View Minimum (originally 80)")]
+    [DefaultValue(80.0f)]
+    public float FieldOfViewMax { get; set; } = 80f;
+}
+
+/// <summary>
+/// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+/// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+/// </summary>
+public class ConfiguratorMixin : ConfiguratorMixinBase
+{
+    // 
+}
---
+++ ff16.qol.adjustablephotomode/Mod.cs
@@ -0,0 +1,170 @@
+﻿using ff16.qol.adjustablephotomode.Configuration;
+using ff16.qol.adjustablephotomode.Template;
+
+using Reloaded.Mod.Interfaces;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Memory.Sigscan;
+using Reloaded.Memory.Sigscan.Definitions.Structs;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
+
+using System.Diagnostics;
+using Reloaded.Memory.Interfaces;
+
+using FF16Framework.Interfaces;
+using FF16Framework.Interfaces.Nex;
+using FF16Framework.Interfaces.Nex.Structures;
+using FF16Tools.Files;
+using FF16Tools.Files.Nex;
+using FF16Tools.Files.Nex.Entities;
+using Syroot.BinaryData.Memory;
+
+using ff16.utility.modloader;
+using ff16.utility.modloader.Interfaces;
+using System.Runtime.InteropServices;
+
+namespace ff16.qol.adjustablephotomode;
+
+/// <summary>
+/// Your mod logic goes here.
+/// </summary>
+public class Mod : ModBase // <= Do not Remove.
+{
+    /// <summary>
+    /// Provides access to the mod loader API.
+    /// </summary>
+    private readonly IModLoader _modLoader;
+
+    /// <summary>
+    /// Provides access to the Reloaded.Hooks API.
+    /// </summary>
+    /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+    private readonly IReloadedHooks? _hooks;
+
+    /// <summary>
+    /// Provides access to the Reloaded logger.
+    /// </summary>
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Entry point into the mod, instance that created this class.
+    /// </summary>
+    private readonly IMod _owner;
+
+    /// <summary>
+    /// Provides access to this mod's configuration.
+    /// </summary>
+    private Config _configuration;
+
+    /// <summary>
+    /// The configuration of the currently executing mod.
+    /// </summary>
+    private readonly IModConfig _modConfig;
+
+    private static IStartupScanner? _startupScanner = null!;
+
+    public WeakReference<INextExcelDBApi> _nexApi;
+
+    public Mod(ModContext context)
+    {
+        _modLoader = context.ModLoader;
+        _hooks = context.Hooks;
+        _logger = context.Logger;
+        _owner = context.Owner;
+        _configuration = context.Configuration;
+        _modConfig = context.ModConfig;
+
+#if DEBUG
+        Debugger.Launch();
+#endif
+
+        _logger.WriteLine($"[{_modConfig.ModId}] Initializing..", _logger.ColorGreen);
+
+        _nexApi = _modLoader.GetController<INextExcelDBApi>();
+        if (!_nexApi.TryGetTarget(out INextExcelDBApi nextExcelDBApi))
+        {
+            _logger.WriteLine($"[{_modConfig.ModId}] Could not get INextExcelDBApi. Is the FFXVI Mod Framework installed/loaded?");
+            return;
+        }
+
+        nextExcelDBApi.OnNexLoaded += NextExcelDBApi_OnNexLoaded;
+    }
+
+    /// <summary>
+    /// Fired when the game has loaded all nex tables.
+    /// </summary>
+    private unsafe void NextExcelDBApi_OnNexLoaded()
+    {
+        ApplyPhotoCameraParamChanges();
+    }
+
+    private unsafe void ApplyPhotoCameraParamChanges()
+    {
+#if DEBUG
+        Debugger.Launch();
+#endif
+
+        if (!_nexApi.TryGetTarget(out var nextExcelDBApi))
+        {
+            _logger.WriteLine($"[{_modConfig.ModId}] Could not get INextExcelDBApi. Is the FFXVI Mod Framework installed/loaded?", _logger.ColorRed);
+            return;
+        }
+
+        if (!nextExcelDBApi.Initialized)
+        {
+            _logger.WriteLine($"[{_modConfig.ModId}] Changes NOT applied, the game hasn't initialized the nex database yet.", _logger.ColorYellow);
+            return;
+        }
+
+        NexTableInstance* photoTable = nextExcelDBApi.GetTable(NexTableIds.photocameraparam);
+        if (photoTable is null)
+        {
+            _logger.WriteLine($"[{_modConfig.ModId}] Unable to fetch table 'photocameraparam'", _logger.ColorRed);
+            return;
+        }
+
+        NexTableLayout layout = TableMappingReader.ReadTableLayout("photocameraparam", new Version(1, 0, 0));
+        for (int i = 0; i < photoTable->NumRows; i++)
+        {
+            NexRowInstance* rowInstance = nextExcelDBApi.SearchRow(photoTable, i);
+            if (rowInstance is null)
+            {
+                _logger.WriteLine($"[{_modConfig.ModId}] Could not get photocameraparam row {i}, skipping", _logger.ColorRed);
+                continue;
+            }
+
+            Nex1KRowInfo* rowInfo = (Nex1KRowInfo*)rowInstance->RowInfo;
+            if (rowInfo->Key1 >= 3)
+                continue;
+
+            byte* rowData = nextExcelDBApi.GetRowData(rowInstance);
+            *(float*)&rowData[layout.Columns["CollisionSphereRadius"].Offset] = _configuration.CollisionSphereRadius;
+            *(float*)&rowData[layout.Columns["CameraMoveSpeed"].Offset] = _configuration.CameraMoveSpeed;
+            *(float*)&rowData[layout.Columns["CameraUpDownSpeed"].Offset] = _configuration.CameraUpDownSpeed;
+            *(float*)&rowData[layout.Columns["CameraUpDownSpeed"].Offset] = _configuration.CameraRotationSpeed;
+            *(float*)&rowData[layout.Columns["FieldOfView"].Offset] = _configuration.FieldOfView;
+            *(float*)&rowData[layout.Columns["FieldOfViewMin"].Offset] = _configuration.FieldOfViewMin;
+            *(float*)&rowData[layout.Columns["FieldOfViewMax"].Offset] = _configuration.FieldOfViewMax;
+        }
+
+        _logger.WriteLine($"[{_modConfig.ModId}] Applied photocameraparam changes.", _logger.ColorGreen);
+    }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        _configuration = configuration;
+        _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+
+        ApplyPhotoCameraParamChanges();
+    }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
+}
